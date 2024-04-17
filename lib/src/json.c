62b56a4a3399deb24a129c036aa641d42323e256
Vinícius Marques <marquesv473@gmail.com>
2024-04-17 18:38:34 -0400

    docs: adicionado comentários simples

--- lib/src/json.c
+++ lib/src/json.c
@@ -15,18 +15,20 @@ int json_parse(FILE * fjson, char *keys[], int n, void (*aloca)(void **reg), voi
     char linha[BUFFER_SIZE];
     while (fgets(linha, BUFFER_SIZE, fjson) != NULL)
     {
+        // Ignora linhas com comentários
         if(strpbrk(linha, "[]") != NULL) {
             continue;
         }
 
+        // Verifica se a linha contém um objeto
         if (strpbrk(linha, "{") != NULL) {
+            // Aloca um novo registro para o objeto
             void *reg = NULL;
             aloca(&reg);
-            if (reg == NULL) {  
-                printf("Erro ao alocar registro\n");        
+            if (reg == NULL) { 
                 return EXIT_FAILURE;
             }
-            printf("Alocando registro\n");
+            // Lê as linhas do objeto
             for (int i = 0; i < n; i++) {
                 fgets(linha, BUFFER_SIZE, fjson);
                 if (strstr(linha, keys[i]) == NULL)
@@ -36,6 +38,14 @@ int json_parse(FILE * fjson, char *keys[], int n, void (*aloca)(void **reg), voi
                 if (value != NULL) {
                     value++;
                     value = ltrim(value);
+                    char *squote = strchr(value, '"');
+                    // if (squote != NULL) {
+                    //     value = squote + 1;
+                    //     char *end = strchr(value, '"');
+                    //     if (end != NULL) {
+                    //         *end = '\0';
+                    //     }
+                    // }
                     char *end = strrchr(value, ',');
                     if (end != NULL && *(end+1) == '\n') {
                         *end = '\0';
